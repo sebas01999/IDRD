65dce6d687b4ed34a8979e3bb54e511e9b1759b6
sebas01999 <zebaz1999@outlook.es>
2022-04-02 12:09:24 -0500

    .

---
+++ app/src/main/java/com/example/idrd/base/BaseActivity.kt
@@ -0,0 +1,27 @@
+package com.example.idrd.base
+
+import android.content.Context
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        super.onCreate(savedInstanceState)
+
+        setContentView(getLayout())
+    }
+
+    @LayoutRes
+    abstract fun getLayout():Int
+
+    fun Context.toast(context: Context = applicationContext, message: String?, duration: Int = Toast.LENGTH_SHORT){
+        Toast.makeText(context, message, duration).show()
+    }
+
+}
