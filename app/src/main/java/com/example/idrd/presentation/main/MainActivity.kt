b55794dde26a81bfc69f6edd7499ea5ac667784a
ValeM131000 <91999897+ValeM131000@users.noreply.github.com>
2022-04-02 12:44:13 -0500

    .

--- app/src/main/java/com/example/idrd/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/idrd/presentation/main/MainActivity.kt
@@ -3,10 +3,32 @@ package com.example.idrd.presentation.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.idrd.R
+import com.example.idrd.base.BaseActivity
+import com.example.idrd.presentation.login.view.LoginFragment
+import com.example.idrd.presentation.main.adapters.viewAdapter
+import com.example.idrd.presentation.register.view.RegisterFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setUpTabs()
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.activity_main
+    }
+
+    private fun setUpTabs(){
+
+        val adapter = viewAdapter(supportFragmentManager)
+        adapter.addFragmentList(LoginFragment(), "Ingresa")
+        adapter.addFragmentList(RegisterFragment(), "Registrate")
+
+        viewpager.adapter= adapter
+        tabs.setupWithViewPager(viewpager)
+
+
     }
 }
---
+++ app/src/main/java/com/example/idrd/presentation/main/adapters/viewAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.idrd.presentation.main.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class viewAdapter( support: FragmentManager): FragmentPagerAdapter(support,
+    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+) {
+
+
+    private val mFragmenList = ArrayList<Fragment>()
+    private val mFragmentTittleList = ArrayList<String>()
+
+    override fun getItem(position: Int): Fragment {
+
+        return mFragmenList[position]
+    }
+
+    override fun getCount(): Int {
+        return mFragmenList.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        return mFragmentTittleList[position]
+    }
+
+    fun addFragmentList(fragment: Fragment, title:String){
+        mFragmenList.add(fragment)
+        mFragmentTittleList.add(title)
+
+    }
+
+}
