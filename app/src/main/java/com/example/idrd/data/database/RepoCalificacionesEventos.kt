51d2c2995efc5c93804b1ea2e330a1d944be6375
Sebastian Salamanca <zebaz1999@outlook.es>
2023-01-24 11:34:10 -0500

    calificaciones

--- app/src/main/java/com/example/idrd/data/database/RepoCalificacionesEventos.kt
+++ app/src/main/java/com/example/idrd/data/database/RepoCalificacionesEventos.kt
@@ -1,4 +1,6 @@
 package com.example.idrd.data.database
 
 class RepoCalificacionesEventos {
+
+
 }
---
+++ app/src/main/java/com/example/idrd/data/model/CalificacionEvento.kt
@@ -0,0 +1,15 @@
+package com.example.idrd.data.model
+
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
+
+class CalificacionEvento {
+    var id:String="DEFAULT ID"
+    var comentario:String = "DEFAULT COMENTARIO"
+    var idParque:String = "DEFAULT IDEVENTO"
+    var idUser:String = "DEFAULT IDUSER"
+    @ServerTimestamp
+    var fecha: Date? = null
+    var estrellas:Int = 1
+
+}
